2c7d0588a7d780327db01609267e4b2005f53f58
Tigran69LYON <tandonian@thenuumfactory.fr>
2023-07-20 14:43:30 +0200

    modif

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour, World Devops ! Je suis content de vous voir!");
+        System.out.println("Bonjour, World Devohdhdhs ! Je suis content de vous voir!");
         Additionner(1, 2);
 }
     public static int Additionner(int a, int b) {
